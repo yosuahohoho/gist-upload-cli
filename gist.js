4fef8b341dfe9ac48b7adb25e0cfdddc4c3be692
yosuahohoho <yosuahohoho@gmail.com>
2018-11-23 15:44:31 +0700

    add fs, request-promise, path  module

--- gist.js
+++ gist.js
@@ -1,6 +1,36 @@
 const rl = require('readline-sync')
+const fs = require('fs')
+const rp = require('request-promise')
+const path = require('path')
 
 const userName = rl.question("Github Username: ")
 const password = rl.question("Github Password: ", {
-  hideEchoBack: true
+  hideEchoBack: false
 })
+
+const filePath = rl.questionPath("File Location: ", {
+  isFile: true
+})
+const fileName = path.posix.basename(filePath)
+
+
+const options = {
+  method: 'POST',
+  uri: 'https://api.github.com/gists',
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:63.0) Gecko/20100101 Firefox/63.0',
+    'Authorization': "Basic " + Buffer.from(`${userName}:${password}`).toString('base64')
+  },
+  body: {
+    'files': {
+      fileName: {
+        'content': fs.createReadStream(filePath)
+      }
+    }
+  },
+  json: true
+}
+
+rp(options)
+    .then(response => console.log(response.status))
+    .catch(err => console.log(err.message))
