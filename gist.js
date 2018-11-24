3bf996272ed9d04049c02d64815617cb340ace2f
yosuahohoho <yosuahohoho@gmail.com>
2018-11-24 10:35:37 +0700

    completed Version 1.0

--- gist.js
+++ gist.js
@@ -7,30 +7,36 @@ const userName = rl.question("Github Username: ")
 const password = rl.question("Github Password: ", {
   hideEchoBack: false
 })
-
 const filePath = rl.questionPath("File Location: ", {
   isFile: true
 })
-const fileName = path.posix.basename(filePath)
+const title = path.posix.basename(filePath)
 
+try {
+  const content = fs.readFileSync(filePath, 'utf8')
 
-const options = {
-  method: 'POST',
-  uri: 'https://api.github.com/gists',
-  headers: {
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:63.0) Gecko/20100101 Firefox/63.0',
-    'Authorization': "Basic " + Buffer.from(`${userName}:${password}`).toString('base64')
-  },
-  body: {
-    'files': {
-      fileName: {
-        'content': fs.createReadStream(filePath)
+  const options = {
+    method: 'POST',
+    uri: 'https://api.github.com/gists',
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:63.0) Gecko/20100101 Firefox/63.0',
+      'Authorization': "Basic " + Buffer.from(`${userName}:${password}`).toString('base64')
+    },
+    body: {
+      'files': {
+        [title]: {
+          'content': content
+        }
       }
-    }
-  },
-  json: true
+    },
+    json: true
+  }
+  
+  rp(options)
+      .then( response => console.log(response['html_url']))
+      .catch( err => console.log(err.message))
+
+} catch(err) {
+  console.log('Error:', err.stack)
 }
 
-rp(options)
-    .then(response => console.log(response.status))
-    .catch(err => console.log(err.message))
